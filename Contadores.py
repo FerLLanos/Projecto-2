ab4fc8b098f86fe93162032c0793c6ff046c139b
FerLLanos <90073117+FerLLanos@users.noreply.github.com>
2021-10-01 21:10:31 -0300

    Add files via upload

---
+++ Contadores.py
@@ -0,0 +1,32 @@
+varNumero1 = 0
+varIncrementador = 1
+
+varNumero1 = varNumero1 + varIncrementador
+
+print(varNumero1) 
+
+print("El pipe puro que desordena")
+
+varNumero1 = varNumero1 + varIncrementador
+
+print(varNumero1) 
+
+print("mendell pecho frio")
+
+varNumero1 = varNumero1 + varIncrementador
+
+print(varNumero1) 
+
+print("----------------------------------------------")
+
+varRango1 = -500
+varRango2 = 0
+
+for varCont in range (varRango1, varRango2):
+    print ("la secuencia es: "+str(varCont))
+
+varArreglo=[a,e,i,o,u]
+
+for varCont1 in varArreglo:
+    print ("la secuencia es: "+str(varCont))
+
